9fe86715aa21362ac4282fa2d94f1081fe3c6c35
Wesley Aspinall <wra427@gmail.com>
2015-11-16 23:29:57 -0500

    fixed list view link to single page

--- js/controllers/singleTodo.controller.js
+++ js/controllers/singleTodo.controller.js
@@ -1,4 +1,4 @@
-let SingleTodoController = function($scope, $stateParams, TodoService, $state,) {
+let SingleTodoController = function($scope, $stateParams, TodoService, $state) {
   
   TodoService.getTodo($stateParams.objectId).then( (res) => {
     $scope.singleTodo = res.data;
